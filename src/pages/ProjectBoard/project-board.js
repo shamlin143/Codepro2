40be19058c01e2d6c1febde15a6afaa620ffb10c
Doug Ritchie <dritchie3@sbcglobal.net>
2020-07-06 20:11:57 -0500

    add functionality to projectFeatures array

--- src/pages/ProjectBoard/project-board.js
+++ src/pages/ProjectBoard/project-board.js
@@ -2,8 +2,6 @@ import React, {useState, useEffect, } from "react";
 import API from "../../utils/API";
 import SubmitProject from "../../components/SubmitProject/submit-project";
 import "./project-board.css";
-// import {Sidebar} from "../../components/sidebar/sidebar"
-
 
 function ProjectBoard(props) {
 console.log(props)
@@ -31,14 +29,14 @@ return(
                 {projects.map(project =>(
                     <div className="cardcontainer bg-success" > 
                         <div className="row mx-5 my-4 ">
-                            <div className="card bg-success">
+                            <div className="card border-success bg-success">
                                 <p key={project._id.projectName}>Project Name: {project.projectName}</p>
                                 <p key={project._id.businessType}>Business Type: {project.businessType}</p>
                                 <p key={project._id.projectDescription}>Project Description: {project.projectDescription}</p>
                                 <p key={project._id.projectFeatures}>Basic Application Features: {project.projectFeatures}</p>
                                 <p key={project._id.projectNotes}>Additional Notes: {project.projectNotes}</p>
-                                <p key={project._id.fee}>Proposed Fee: ${project.fee}</p><br></br>   
-                                <p key={project._id.email}>Contact Email: {project.email}</p><br></br>   
+                                <p key={project._id.fee}>Proposed Fee: ${project.fee}</p> 
+                                <p key={project._id.email}>Contact Email: {project.email}</p>
                             </div>
                         </div>
                     </div>
