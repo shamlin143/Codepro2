d5bc40f21b4d12ac3fe0722f01a89deb4d63a983
Doug Ritchie <dritchie3@sbcglobal.net>
2020-07-10 10:03:06 -0500

    package.json for heroku?

---
+++ packagelock.js
@@ -0,0 +1,9 @@
+// "scripts": {
+//     "start:dev": "concurrently \"npm run server\" \"npm run client\"",
+//     "start": "npm run server",
+//     "client": "cd client && npm start",
+//     "server": "node server.js",
+//     "build": "cd client && npm run build",
+//     "heroku-postbuild": "cd client && npm install && npm run build "
+//   },
+
